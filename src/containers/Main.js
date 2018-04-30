c72904290bf95cdbc87eb95ae2daade7d3518c24
Fernando Maymone <fmaymone@gmail.com>
2018-04-30 11:48:11 -0300

    MOD changed breaking state

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -14,7 +14,7 @@ class Main extends React.Component {
       isLoading: false
     };
   }
-  componentWillMount() {
+  componentDidMount() {
     this.setState({ isLoading: true });
     this.loadData();
   }
